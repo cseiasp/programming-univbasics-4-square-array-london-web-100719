73a85352bd19ccecd9568d42f246f0d4eae20ed5
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-05 08:44:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   while counter <= array.length 
-  new_array << array[counter] ** 2 
+  new_array << array[counter] * array[counter] 
   counter += 1 
 end
 end
